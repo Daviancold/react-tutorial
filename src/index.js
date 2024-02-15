7992c2b5f42a8ea4963a60ca9a28f3fac7b3df9d
Daviancold <110613918+Daviancold@users.noreply.github.com>
2024-02-16 02:57:21 +0800

    Add new components to index.js
    Things Learnt: making basic react components, CSS, JSX-CSS,
    JSX-JavaScript, Props and Simple list

--- src/index.js
+++ src/index.js
@@ -1,17 +1,44 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import './index.css'
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const books = [
+  {
+    title: 'The Women: A Novel',
+    author: 'Kristin Hannah',
+    img: './images/thewomennovel.jpg',
+    id: 1,
+  },
+  {
+    title: 'The Holy Grail of Investing',
+    author: 'Tony Robbins',
+    img: 'https://images-na.ssl-images-amazon.com/images/I/71zQ-0+fV7L._AC_UL600_SR600,400_.jpg',
+    id: 2,
+  }
+];
+
+const BookList = () => {
+  return (
+    <section className="booklist">
+      {books.map((book) => {
+        return <Book {...book} key={book.id} /> // HTML "Arguments"
+      })}
+    </section>
+  )
+}
+
+const Book = (props) => {
+  const { img, title, author } = props;
+  return (
+    <article className="book">
+      <img src={img} alt={title} />
+      <h2>{title}</h2>
+      <h4>{author}</h4>
+    </article>
+  )
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+root.render(<BookList />)
